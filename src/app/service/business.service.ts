d2abf1ea3e9b6f0195b62b6e90ec1be04e83f9fc
barryroubaix <62664220+barryroubaix@users.noreply.github.com>
2023-06-06 16:39:34 -0400

    little bit of code cleanup

--- src/app/service/business.service.ts
+++ src/app/service/business.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Business } from '../lib/business';
-import { BUSINESSES } from '../lib/businesses';
 import { Observable, map } from 'rxjs';
 import { Template } from '../lib/template';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -15,22 +14,6 @@ export class BusinessService {
     constructor(
         private httpClient: HttpClient
     ) { }
-
-    getAllBusinesses(): any {
-        // Construct API call here
-    }
-
-    getBusinessList(templateId: number): any {        // 'getBusinessList' takes in a templateId (from route params) and returns a list of businesses that share that templateId
-        const businessesList = BUSINESSES.filter((business: Business) => {      // *** FUNCTION RETRIEVES BUSINESS DATA FROM LOCAL MEMORY, NOT FROM DATABASE ***
-            return business.templateId == templateId;
-        });
-
-        this.get(templateId).subscribe(businesses => {
-            console.log(businesses)
-        })
-
-        return businessesList;
-    }
 
     public get(templateId: number): Observable<Business[]> {
 
