d6b15c74db42aa4db8bcf77d6b984cb893ef5431
barryroubaix <62664220+barryroubaix@users.noreply.github.com>
2023-06-06 08:02:23 -0400

    add routing if no template id is given in the URL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BusinessListComponent } from './business-list/business-list.component';
 
-const routes: Routes = [               // This route will eventually be something more like a 'dashboard' 
-  { path: 'businesses/:templateId', component: BusinessListComponent}   // Selecting a Template ID from the dropdown will navigate to this route, which will display the businesses that share that Template ID
+const routes: Routes = [               // This route will eventually be something more like a 'dashboard'
+    { path: 'businesses', component: BusinessListComponent },
+    { path: 'businesses/:templateId', component: BusinessListComponent }, // Selecting a Template ID from the dropdown will navigate to this route, which will display the businesses that share that Template ID
 ];
 
 @NgModule({
