4f66b3548d9295a08547154f6fba07f35d676802
barryroubaix <62664220+barryroubaix@users.noreply.github.com>
2023-06-07 17:01:20 -0400

    added image to business list

--- src/app/business-list/business-list.component.ts
+++ src/app/business-list/business-list.component.ts
@@ -7,36 +7,37 @@ import { Location } from '@angular/common';
 import { environment } from 'src/environments/environment';
 
 @Component({
-    selector: 'app-business-list',
-    templateUrl: './business-list.component.html',
-    styleUrls: ['./business-list.component.css']
+  selector: 'app-business-list',
+  templateUrl: './business-list.component.html',
+  styleUrls: ['./business-list.component.css'],
 })
 export class BusinessListComponent implements OnInit {
-
   constructor(
     private activatedRoute: ActivatedRoute,
     private businessService: BusinessService,
     private location: Location
-  ) { }
+  ) {}
 
-  businesses: Business[] = [];      // initializing a Business[] type variable, for use in the HTML
+  businesses: Business[] = []; // initializing a Business[] type variable, for use in the HTML
 
-  metroAliveURL = environment.metroAliveURL
+  metroAliveURL = environment.metroAliveURL;
+  imageFileURL = environment.imageFileURL;
 
   ngOnInit(): void {
     this.activatedRoute.url.subscribe((url: UrlSegment[]) => {
-      this.getBusinessList(Number(url[1]?.path))
-    }) // watch for changes to the URL
+      this.getBusinessList(Number(url[1]?.path));
+    }); // watch for changes to the URL
   }
 
-  getBusinessList(templateId: number): void {          // Calling this function generates a list of Businesses that share a template based on route parameters
-      // assigning the output of businessService as a Business[] interface
-      this.businessService.get(templateId).subscribe((businesses: Business[]) => {
-        this.businesses = businesses
-      })
+  getBusinessList(templateId: number): void {
+    // Calling this function generates a list of Businesses that share a template based on route parameters
+    // assigning the output of businessService as a Business[] interface
+    this.businessService.get(templateId).subscribe((businesses: Business[]) => {
+      this.businesses = businesses;
+    });
   }
 
   goBack(): void {
-      this.location.back();
+    this.location.back();
   }
 }
