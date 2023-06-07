46a340dd7d4e45dc01795ccdda8987ae4178832f
Dane <danefalberg@gmail.com>
2023-06-07 13:27:19 -0400

    Converted business-list to Material. Will continue to adjust

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
 
 @NgModule({
@@ -30,6 +31,7 @@ import { NgModule } from '@angular/core';
     MatIconModule,
     MatButtonModule,
     MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
